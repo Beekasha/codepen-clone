8655b1eac98daa1d64f10ef565e74a8248baf942
Beekasha <brian.j.kleiner@gmail.com>
2020-09-19 19:42:03 -0400

    Add Editor Component and iframe

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import Editor from './Editor'
 
 function App() {
   return (
-    "Hello World"
+    <>
+      <div className="pane top-pane">
+        <Editor />
+        <Editor />
+        <Editor />
+      </div>
+      
+      <div className="pane">
+        <iframe title="output"
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
   );
 }
 
